da3fd0956c9d4ec20e188ce5ed1352334c6d263d
KenHaya <hkearth777@gmail.com>
2022-05-01 19:39:03 +0900

    Deviseの導入に伴いマイグレションファイルの実行ができない

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -7,4 +7,5 @@ class HomeController < ApplicationController
 
   def about
   end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
