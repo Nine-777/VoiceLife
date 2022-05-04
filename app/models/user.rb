bf1db8a221ba4ad1cf0653691437b46e6793dfdf
KenHaya <hkearth777@gmail.com>
2022-05-04 13:09:17 +0900

    ユーザーと投稿の紐付け

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,9 @@ class User < ApplicationRecord
     has_secure_password
     validates :name, {presence: true}
     validates :email, {presence: true, uniqueness: true}
+
+    def posts
+        return Post.where(user_id: self.id)
+    end
+
 end
