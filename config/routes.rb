6b7d2c33097509e02613e86a2cf90e43c5ac1111
Nine <97382295+Nine-777@users.noreply.github.com>
2022-05-08 18:27:52 +0900

    Update routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   post "likes/:post_id/create" => "likes#create"
   post "likes/:post_id/destroy" => "likes#destroy"
+  get "users/:id/likes" => "users#likes"
   get "login" => "users#login_form"
   post "login" => "users#login"
   post "logout" => "users#logout"
