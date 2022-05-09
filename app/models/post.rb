fd91c51770b65fc1a44622c2006432c55af173e1
KenHaya <hkearth777@gmail.com>
2022-05-09 20:16:59 +0900

    carrierwave導入

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ApplicationRecord
     validates :content, {presence: true, length: {maximum: 100}}
     validates :user_id, {presence: true}
+    mount_uploader :file, AudiofileUploader
 
     def user
         return User.find_by(id: self.user_id)
